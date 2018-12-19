655a623985fbd23eadd9186cf15fdcbc5b11710c
Jeevan MB <jforcode@Jeevans-MacBook-Pro.local>
2018-12-20 00:02:42 +0530

    removed transactions in table name. returning result from prepare and exec. added db compare function

--- dbUtil.go
+++ dbUtil.go
@@ -2,8 +2,10 @@ package util
 
 import (
 	"database/sql"
+	"fmt"
 	"strings"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/jforcode/DeepError"
 )
 
@@ -11,7 +13,8 @@ type IDbUtil interface {
 	GetDb(user, password, host, database string, flagsMap map[string]string) (*sql.DB, error)
 	ClearTables(db *sql.DB, tables ...string) error
 	GetRowCount(db *sql.DB, table string, where string, params []interface{}) (int, error)
-	PrepareAndExec(db *sql.DB, query string, parameters ...interface{}) (int64, error)
+	PrepareAndExec(db *sql.DB, query string, parameters ...interface{}) (sql.Result, error)
+	CompareDbData(db *sql.DB, query string, args []interface{}, numCols int, expected [][]string, debug bool) (bool, error)
 }
 
 type DbUtil struct{}
@@ -56,25 +59,9 @@ func (dbUtil *DbUtil) GetDb(user, password, host, database string, flagsMap map[
 func (dbUtil *DbUtil) ClearTables(db *sql.DB, tables ...string) error {
 	fnName := "util.DbUtil.ClearTables"
 
-	tx, err := db.Begin()
-	if err != nil {
-		return deepError.New(fnName, "tx begin", err)
-	}
-
 	for _, table := range tables {
 		_, err := db.Exec("DELETE FROM " + table)
 		if err != nil {
-			err2 := tx.Rollback()
-			if err2 != nil {
-				return deepError.DeepErr{
-					Function: fnName,
-					Action:   "rolling back transaction",
-					Message:  "Couldn't roll back transaction for table",
-					Params:   []interface{}{table},
-					Cause:    err2,
-				}
-			}
-
 			return deepError.DeepErr{
 				Function: fnName,
 				Action:   "exec",
@@ -83,11 +70,6 @@ func (dbUtil *DbUtil) ClearTables(db *sql.DB, tables ...string) error {
 				Cause:    err,
 			}
 		}
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return deepError.New(fnName, "commiting transaction", err)
 	}
 
 	return nil
@@ -119,23 +101,59 @@ func (dbUtil *DbUtil) GetRowCount(db *sql.DB, table string, where string, params
 	return count, nil
 }
 
-func (dbUtil *DbUtil) PrepareAndExec(db *sql.DB, query string, parameters ...interface{}) (int64, error) {
+func (dbUtil *DbUtil) PrepareAndExec(db *sql.DB, query string, parameters ...interface{}) (sql.Result, error) {
 	fnName := "util.dbUtil.PrepareAndExec"
 
 	stmt, err := db.Prepare(query)
 	if err != nil {
-		return -1, deepError.New(fnName, "preparing", err)
+		return nil, deepError.New(fnName, "preparing", err)
 	}
 
 	res, err := stmt.Exec(parameters...)
 	if err != nil {
-		return -1, deepError.New(fnName, "exec", err)
+		return nil, deepError.New(fnName, "exec", err)
 	}
 
-	numInserted, err := res.RowsAffected()
+	return res, nil
+}
+
+func (dbUtil *DbUtil) CompareDbData(db *sql.DB, query string, args []interface{}, numCols int, expected [][]string, debug bool) (bool, error) {
+	fn := "CompareDbData"
+	rows, err := db.Query(query, args...)
 	if err != nil {
-		return -1, deepError.New(fnName, "getting rows affected", err)
+		return false, deepError.New(fn, "query", err)
+	}
+	defer rows.Close()
+
+	actual := make([][]string, 0)
+	for rows.Next() {
+		rowData := make([]string, numCols)
+		rowDataPtr := make([]interface{}, numCols)
+		for i := 0; i < numCols; i++ {
+			rowDataPtr[i] = &rowData[i]
+		}
+
+		err := rows.Scan(rowDataPtr...)
+		if err != nil {
+			return false, deepError.New(fn, "scan", err)
+		}
+
+		actual = append(actual, rowData)
 	}
 
-	return numInserted, nil
+	if debug {
+		fmt.Printf("\nActual\n%+v\nExpected\n%+v\n", actual, expected)
+	}
+
+	if len(actual) != len(expected) {
+		return false, nil
+	}
+
+	for ind, act := range actual {
+		if !cmp.Equal(act, expected[ind]) {
+			return false, nil
+		}
+	}
+
+	return true, nil
 }
